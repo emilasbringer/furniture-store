2556c8db4165bc10861f12e877d36be670fe849d
emilasbringer <60388760+emilasbringer@users.noreply.github.com>
2025-05-13 20:18:00 +0200

    prod final test

--- src/components/Newsbanner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Newsbanner() {
-    return (
-    <div className="w-full bg-red-100">
-        <h1 className="font-bold text-center py-1 flex justify-center">15% rabbat med koden <p className="ml-1 font italic">CASPAR ÄR ETT OLLON</p></h1>
-    </div>
-    )
-}
